8ff991d4e3905d028316a2cbbf9b4346059baf39
Olav Henriksen <oh@forskningsradet.no>
2014-09-22 18:25:57 +0200

    Lagt inn timer i timeglassview

--- First Charade/First Charade/TimeglassViewController.swift
+++ First Charade/First Charade/TimeglassViewController.swift
@@ -9,23 +9,45 @@
 import UIKit
 
 class TimeglassViewController: UIViewController {
+    @IBOutlet var tidIgjenLabel : UILabel!
+    var timer = NSTimer()
+    var tidIgjen = 10
     
-    @IBAction func naviger() {
-        self.performSegueWithIdentifier("vunnet", sender: self)
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        visGjenståendeTid()
+        let updateTime : Selector = "oppdaterGjenstaendeTid"
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: updateTime, userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        timer.invalidate()
     }
     
+    func oppdaterGjenstaendeTid() {
+        tidIgjen--
+        if (tidIgjen <= 0) {
+            tapt()
+        }
+        visGjenståendeTid()
+    }
+    
+    func visGjenståendeTid() {
+        tidIgjenLabel.text = String(tidIgjen)
+    }
+    
+    @IBAction func vunnet() {
+        self.performSegueWithIdentifier("vunnet", sender: self)
+    }
 
+    func tapt() {
+        self.performSegueWithIdentifier("tapt", sender: self)
+    }
+    
     /*
     // MARK: - Navigation
 
