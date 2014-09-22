2bc4355bf21215266e17bc01494931c3cd3aaa88
MikkelSteine <deepskygazer@gmail.com>
2014-09-22 16:40:57 +0200

    Ny klasse for innstillinger

---
+++ First Charade/First Charade/InstillingerViewController.swift
@@ -0,0 +1,33 @@
+//
+//  InstillingerViewController.swift
+//  First Charade
+//
+//  Created by Mikkel Steine on 22/09/14.
+//  Copyright (c) 2014 Mesan. All rights reserved.
+//
+
+import UIKit
+
+class InstillingerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+
+}
